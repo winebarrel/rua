6462fa490f8a8bf9a6d4420b796751e6430992f6
winebarrel <sgwr_dts@yahoo.co.jp>
2011-01-09 20:23:57 +0900

    check liblua5.1 and liblua

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 dir_config('lua', '/usr/include/lua5.1', '/usr/lib')
-if have_header('lua.h') and have_header('lualib.h') and have_header('lauxlib.h') and have_library('lua5.1') and have_header('iconv.h') and have_header('errno.h')
+if have_header('lua.h') and have_header('lualib.h') and have_header('lauxlib.h') and (have_library('lua5.1') or have_library('lua')) and have_header('iconv.h') and have_header('errno.h')
   have_header('iconv')
   create_makefile('rua')
 end
